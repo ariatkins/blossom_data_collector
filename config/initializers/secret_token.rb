d8af34d86895f8992e7bcb2577257aaaee24cab6
Ari Atkins <ari@unpluggedinstruments.com>
2012-10-03 11:47:31 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlossomDataCollector::Application.config.secret_token = '6b0d1e0a60970d4afc379c0ee551f9d5247865cc0dd90b9654fbbffd48e440771436fe048523d818ca248da5fa2d2e1711a6a07128d55d6254c2588d46df9730'
