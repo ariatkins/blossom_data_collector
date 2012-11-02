04689c30b25ae1e472c0f31c7e20a7667c7d0c13
Ari Atkins <ari@unpluggedinstruments.com>
2012-11-02 11:52:28 -0700

    fix poweron

--- app/controllers/powerons_controller.rb
+++ app/controllers/powerons_controller.rb
@@ -3,7 +3,7 @@ class PoweronsController < ApplicationController
 	def create
 		data = params[:value].split(',')
 		@machine = Machine.find_by_imp_id(data[0])
-		@poweron = @machine.cups.new(time: data[1])
+		@poweron = @machine.powerons.new(time: data[1])
 		@poweron.save
 		render :text=>"ok"
 	end
--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,6 +2,7 @@ class Machine < ActiveRecord::Base
   attr_accessible :imp_id
   belongs_to :user
   has_many :cups
+  has_many :powerons
 
   validates :imp_id, presence: true
   validates :user_id, presence: true
